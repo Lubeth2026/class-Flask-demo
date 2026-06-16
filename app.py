d618aff12390b84bd80bbb6e5c2c474590263bb6
Lubeth <lubethjimenez@yahoo.com>
2026-06-15 20:22:27 -0400

    Add examples of building API's with Python

---
+++ app.py
@@ -0,0 +1,31 @@
+
+# Building an API
+from flask import Flask
+from pokemon_data import pokemon_response
+
+# If you run app.py directly __name__ == __main__ (ONLY in this scenario type)
+app = Flask(__name__)
+
+# DICTIONARY
+banana = {
+    "test": "ok",
+    "banana": True,
+    "count": 1
+}
+
+# Endpoint for the Root Route to name/main 
+# DECORATOR
+@app.route("/")
+def home():
+    return banana 
+
+# Create a Proxy-Key
+@app.route("/get-key")
+def get_key():
+    return {"key": "ciecgueg857439284nvbdd"}
+
+# Lewis Created his own Pokemon API
+null = None 
+@app.rpute("/pokemon")
+def get_pokemon():
+    return pokemon_response
